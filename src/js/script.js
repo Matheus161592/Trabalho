461006d5e19b41e724842993acfa0f8cf99d9ec1
Matheus161592 <matheus_5k@outlook.com>
2025-05-15 05:55:05 -0300

    Add files via upload

---
+++ src/js/script.js
@@ -0,0 +1,6 @@
+let botao= document.querySelector("#botao");
+
+botao.addEventListener("click",e =>{
+    botao.innerHTML ="Aja como se o que você faz fizesse a diferença. Porque realmente faz.";
+    
+});
